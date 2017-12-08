a11ca71ca4062075d4520b78573ac75b0b8d5def
Rudolfo de Sousa de Oliveira <rudolfosousa@gmail.com>
2017-12-07 22:49:25 -0200

    update mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,64 @@
-function openModal(){
-	$(".player-video").on("click", function(){
+var Index = {
+
+	// Abre modal
+	openModal: function(){
+		$(".player-video").on("click", function(){
 		$(".modal").addClass("active");
 		$(".modal").animate({
 			opacity: "1"
-		})
+			})
+		});	
+	},
+
+	// Fecha modal
+
+	closeModal: function(){
+		$(".close-modal").on("click", function(){
+			$(".modal").animate({
+				opacity: "0"
+			}, 500, function(){
+				$(".modal").removeClass("active");
+			})
+		});	
+	},
+
+	// abre menu
+
+	openMenu:function(){
+		$(".menu-open").on("click", function(){
+			$('html').addClass('active');
+		});
+	},
+
+	// fecha menu
+
+	closeMenu:function(){
+		$(".close-menu").on('click', function(){
+			$('html').removeClass('active');
+		})	
+	},
+
+	// Scroll da pagina
+
+	scrollWindow: function(){
+		$(".menu-item a").click(function(event){
+		$('html,body').animate({scrollTop:$(this.hash).offset().top},1000, function(){
+			$(this).removeClass('active');
+		});
 	});
+	}, 
+
+	init:function(){
+		this.scrollWindow();
+		this.closeModal();
+		this.openModal();
+		this.openMenu();
+		this.closeMenu();
+	}
 }
 
-function closeModal(){
-	$(".close-modal").on("click", function(){
-		$(".modal").animate({
-			opacity: "0"
-		}, 500, function(){
-			$(".modal").removeClass("active");
-		});
-	})
-}
+// Fim objeto
+
 $(document).ready(function(){
-	openModal();
-	closeModal();
+	Index.init();
 })
